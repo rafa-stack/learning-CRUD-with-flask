07fcef8d8bc8038c1abcddeaf2d5879830a7c33f
rafa-stack <r4f4.aulia@gmail.com>
2025-10-12 23:46:23 +0700

    update models py and instance

--- models.py
+++ models.py
@@ -4,10 +4,12 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True)
-    password = db.Column(db.String(200))
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    berita = db.relationship('Berita', backref='user', lazy=True)
 
 class Berita(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    judul = db.Column(db.String(200))
-    isi = db.Column(db.Text)
+    judul = db.Column(db.String(200), nullable=False)
+    isi = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False) 
